88a2a7862480648e3a0af0ab8780d202c1014b9c
jgonzalezsolis <jose_5lic@hotmail.com>
2023-12-05 22:34:28 -0700

    new algo

--- algorithms.js
+++ algorithms.js
@@ -90,3 +90,9 @@ function boolToWord( bool ){
             return "Invalid operation";
         }
         }
+
+
+        // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+        function removeExclamationMarks(s) {
+            return s.replace(/!/g, '');
+            }
