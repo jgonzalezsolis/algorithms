27a2276a9b49bc53558ca7dadd3dc7605aee2bfa
jgonzalezsolis <jose_5lic@hotmail.com>
2023-12-05 22:25:15 -0700

    new algo

--- algorithms.js
+++ algorithms.js
@@ -59,3 +59,34 @@ function updateLight(current) {
         return "green"
     }
 }
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+function boolToWord( bool ){
+    return bool?
+    "Yes": "No"
+    }
+
+
+    // Your task is to create a function that does four basic mathematical operations.
+
+    // The function should take three arguments - operation(string/char), value1(number), value2(number).
+    // The function should return result of numbers after applying the chosen operation.
+
+    function basicOp(operation, value1, value2) {
+        switch(operation) {
+            case '+':
+            return value1 + value2;
+            case '-':
+            return value1 - value2;
+            case '*':
+            return value1 * value2;
+            case '/':
+            if(value2 !== 0) {
+                return value1 / value2;
+            } else {
+                return "Cannot divide by zero";
+            }
+        default:
+            return "Invalid operation";
+        }
+        }
